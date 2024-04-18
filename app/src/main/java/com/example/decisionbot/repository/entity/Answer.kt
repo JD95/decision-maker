9692814b6815249a763f341cae2795aee55c2a6f
Jeffrey Dwyer <jeffreydwyer95@gmail.com>
2024-04-17 18:02:55 -0700

    Adds foreign key constraints on entities

--- app/src/main/java/com/example/decisionbot/repository/entity/Answer.kt
+++ app/src/main/java/com/example/decisionbot/repository/entity/Answer.kt
@@ -2,9 +2,19 @@ package com.example.decisionbot.repository.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = Choice::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("id"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
 data class Answer(
     @PrimaryKey val id: Long,
     @ColumnInfo(name = "choice") val choice: Long,
--- app/src/main/java/com/example/decisionbot/repository/entity/Requirement.kt
+++ app/src/main/java/com/example/decisionbot/repository/entity/Requirement.kt
@@ -2,9 +2,25 @@ package com.example.decisionbot.repository.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = Choice::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("choice"),
+            onDelete = ForeignKey.CASCADE
+        ),
+        ForeignKey(
+            entity = Answer::class,
+            parentColumns = arrayOf("id"),
+            childColumns = arrayOf("answer"),
+            onDelete = ForeignKey.CASCADE
+        ),
+    ]
+)
 @kotlinx.serialization.Serializable
 data class Requirement(
     @PrimaryKey val id: Long,
